8e1c9f659dde95d5a4d513c032f91fdfbd52d240
Kwaku <kwakugyapong219@gmail.com>
2021-01-19 11:27:41 -0500

    Zoo

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,3 +1,33 @@
+# - An `Animal` should be instantiated with the species (e.g. "Lion", "Koala", "Lynx") and name (e.g. "Mufasa", "Simba", "Fluffy"). 
+# - `Animal#zoo` should return the zoo instance that the animal can be found in.
+# - `Animal#origin` should return the origin instance that the animal can be found in.
+# - `Animal#species` should return the species of the animal.
+# - `Animal#name` should return the name of the animal.
+# - `Animal.all` should return an array of all the animal instances.
+
+
+
+
+
 class Animal
+
+    attr_reader :species, :name, :origin, :zoo
+
+    @@all = []
+
+    def initialize (species,name, zoo, origin)
+        @species= species
+        @name= name
+        @zoo= zoo
+        @origin= origin
+       
+        @@all << self
+
+
+    end
+
+    def self.all
+      @@all
+    end
 
 end
--- app/models/zoo.rb
+++ app/models/zoo.rb
@@ -1,3 +1,48 @@
+# A `Zoo` should be initialized with a name and address, which should be in passed as string. Keep in mind that the Zoo's name should be able to change, but its address cannot.
+# - `Zoo#name` should return the name of the zoo instance. 
+# - `Zoo#address` should return the address of the zoo instance.
+# - `Zoo.all` should return an array of all the zoo instances.
+# - `Zoo#all_animals` should return all the animals that a specific instance of a zoo has.
+# - `Zoo#all_animal_species` should return an array of the unique animal species that a specific instance of a zoo has.
+# - `Zoo.find_by_name` should take in an argument of a string of a name and return an instance of the zoo.
+# `Zoo#origins` should return all the unique animal origins that a specific instance of a zoo has.
+# ​
+# There are no `rspec` files, so it's up to you to test your code within the `console.rb` file.
+
+
+
+
 class Zoo
+
+    attr_reader :name, :address
+
+   @@all = []
+
+
+    def initialize (name, address)
+        @name= name
+        @address= address
+
+
+        @@all << self 
+    end
+
+
+    def self.all
+        @@all
+    end
+ 
+    def all_animals
+        
+        Animal.all.select{|animal| animal.zoo == self}
+    end
+
+    def all_animal_species
+
+
+        self.all_animals.map{|animal|animal.species}.uniq
+    end
     
+
+
 end
